231c09d9e45b1c46b098a511838cd351580ac688
Kyle Smith <kyleafmine@gmail.com>
2024-06-18 12:40:58 -0300

    Finish MVP?

--- mapperd/Routes/WebsocketJob.cs
+++ mapperd/Routes/WebsocketJob.cs
@@ -1,3 +1,7 @@
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using mapperd.Model;
 
 namespace mapperd.Routes;
@@ -19,21 +23,39 @@ public class WebsocketJob : IHostedService
         return Task.CompletedTask;
     }
 
-    private ArraySegment<Byte> buffer = new ArraySegment<byte>(new byte[8192]);
+    private ArraySegment<Byte> buffer = new(new byte[8192]);
+    
     private async void Recieve()
     {
         while (_running)
         {
             // Check pending connections
-            if (_manager.PendingSockets.Count > 0)
+            foreach (var pending in _manager.PendingSockets) 
             {
-                var socket = _manager.PendingSockets[0];
-                // recieve a message if there's one waiting
-                var token = new CancellationToken();
-                var result = socket.Socket.ReceiveAsync(buffer, token);
-                if (!result.IsCompleted)
+                if (pending.RecvTask.IsCompleted)
                 {
-                    
+                    var result = await pending.RecvTask;
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        _manager.PendingSockets.Remove(pending);
+                        continue;
+                    }
+                    var message = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+                    var msg = JsonSerializer.Deserialize<Message>(message);
+                    if (msg.Op == OpCode.Init)
+                    {
+                        var con = _manager.ReserveConnection();
+                        var response = new Message
+                        {
+                            Op = OpCode.ConnectionId,
+                            Data = JsonValue.Create(con.Id)
+                        };
+                        var responseBytes = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
+                        await pending.Socket.SendAsync(new ArraySegment<byte>(responseBytes), WebSocketMessageType.Text, 
+                            true, CancellationToken.None);
+                        _manager.PendingSockets.Remove(pending);
+                        _manager.Connections.Add(con.Id, con);
+                    }
                 }
             }
             
